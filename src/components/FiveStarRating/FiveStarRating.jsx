6292def57c7f16786f529b3c2839d5c478447b3b
Atef Lachhab <atef.lachhab1@gmail.com>
2024-02-22 10:49:07 -0500

    Etoiles faits en dur

---
+++ src/components/FiveStarRating/FiveStarRating.jsx
@@ -0,0 +1,13 @@
+import s from "./style.module.css";
+import { StarFill, Star as StarEmpty, StarHalf } from "react-bootstrap-icons";
+export function FiveStarRating({ rating }) {
+  return (
+    <div>
+      <StarFill />
+      <StarFill />
+      <StarFill />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
+}

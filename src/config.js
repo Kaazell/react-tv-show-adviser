7bc7a11f9db46bf1813d325d989e7799051fca40
Atef Lachhab <atef.lachhab1@gmail.com>
2024-02-22 10:33:17 -0500

    Projet 50%

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=be7c5d12837b19e468b2262807f6a38c";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL };
